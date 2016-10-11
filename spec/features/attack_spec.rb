51c52e40a7b7b0d9f67d8081de1d02667d2eb6a9
david anderson <d.a.m.anderson@gmail.com>
2016-10-11 14:37:34 +0100

    Feature test for reducing HP

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
-feature 'Player 1 can attack Player 2' do
+feature 'Player 1 can attack Player 2, and reduce HP by 10' do
   scenario 'on welcome screen' do
     sign_in_and_play
     click_button 'Attack Player 2'
-    expect(page).to have_text 'Player 2 has been attacked.'
+    expect(page).to have_text 'david has been attacked. New HP: 90'
   end
 end
