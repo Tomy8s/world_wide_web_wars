43673d2e9783e7f49067c8ed64ff5d15e042c8e7
david anderson <d.a.m.anderson@gmail.com>
2016-10-11 16:22:19 +0100

    SRP refactor

---
+++ spec/game_spec.rb
@@ -0,0 +1,21 @@
+require 'game'
+
+describe Game do
+  subject(:game) { described_class.new(player_1, player_2) }
+  let(:player_1) { double :player }
+  let(:player_2) { double :player }
+
+  describe '#initialize' do
+    it 'is expected to store player variable' do
+      expect(game.player_1).to eq player_1
+    end
+  end
+
+
+  describe '#attack' do
+    it 'damages the player' do
+      expect(player_2).to receive(:receive_damage)
+      game.attack(player_2)
+    end
+  end
+end
