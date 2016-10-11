034c6c8b2c640829b4a0ecba63aa44de02911e88
Tomy8s <tomy8s@hotmail.com>
2016-10-11 12:11:22 +0100

    added web_helpers

--- spec/features/name_form_spec.rb
+++ spec/features/name_form_spec.rb
@@ -2,10 +2,7 @@ require 'spec_helper'
 
 feature 'accepts name' do
   scenario 'submitting names' do
-    visit ('/')
-    fill_in :player_1_name, with: 'david'
-    fill_in :player_2_name, with: 'tom'
-    click_button 'Submit'
-    expect(page).to have_text('Welcome David and Tom!')
+    sign_in_and_play
+    expect(page).to have_text('Welcome Tom and David!')
   end
 end
--- spec/features/see_hp_spec.rb
+++ spec/features/see_hp_spec.rb
@@ -2,10 +2,7 @@ require 'spec_helper'
 
 feature 'Can view player HP' do
   scenario 'on welcome screen' do
-    visit ('/')
-    fill_in :player_1_name, with: 'tom'
-    fill_in :player_2_name, with: 'david'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_text("Tom has 100 HP.\nDavid has 100 HP.")
   end
 end
