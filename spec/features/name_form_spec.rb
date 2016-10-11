82169e4760830eb5de032114444533fdf49534bd
david anderson <d.a.m.anderson@gmail.com>
2016-10-11 11:09:56 +0100

    Fixed config problem

--- spec/features/name_form_spec.rb
+++ spec/features/name_form_spec.rb
@@ -1,11 +1,11 @@
-require 'capybara/dsl'
+require 'spec_helper'
 
 feature 'accepts name' do
   scenario 'submitting names' do
     visit ('/')
-    fill_in 'Player 1', with: 'david'
-    fill_in 'Player 2', with: 'tom'
+    fill_in :player_1_name, with: 'david'
+    fill_in :player_2_name, with: 'tom'
     click_button 'Submit'
-    expect('localhost:4567/name').to have_text('Welcome David and Tom!')
+    expect(page).to have_text('Welcome David and Tom!')
   end
 end
