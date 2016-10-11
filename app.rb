bc7685740f9e34d7bda49bd3ebe4f2fc746d13c9
Tomy8s <tomy8s@hotmail.com>
2016-10-11 11:25:38 +0100

    accepts names on '/' and displays on '/names'

--- app.rb
+++ app.rb
@@ -2,7 +2,13 @@ require 'sinatra/base'
 
 class WorldWebWars < Sinatra::Base
   get '/' do
-    'Hello world_web_wars!'
+    erb(:index)
+  end
+
+  post '/names' do
+    @player_1_name = params[:player_1_name].capitalize
+    @player_2_name = params[:player_2_name].capitalize
+    erb(:play)
   end
 
   # start the server if ruby file executed directly
