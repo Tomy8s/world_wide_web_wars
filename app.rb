0e47b0abe50ba672acc292e859e288da4fd1c519
david anderson <d.a.m.anderson@gmail.com>
2016-10-11 11:45:58 +0100

    Post redirect

--- app.rb
+++ app.rb
@@ -1,15 +1,25 @@
 require 'sinatra/base'
 
 class WorldWebWars < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name].capitalize
-    @player_2_name = params[:player_2_name].capitalize
-    erb(:play)
+    session[:player_1_name] = params[:player_1_name].capitalize
+    session[:player_2_name] = params[:player_2_name].capitalize
+    redirect '/play'
   end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    erb :play
+  end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
