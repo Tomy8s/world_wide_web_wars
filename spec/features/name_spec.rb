8b5b83c25c8cbd32cdb7f46709c841bd11aa5076
Tomy8s <tomy8s@hotmail.com>
2016-10-12 11:14:48 +0100

    players can enter names on index and have them displayed on names

--- spec/features/name_spec.rb
+++ spec/features/name_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 feature 'names' do
   scenario 'Players can input names' do
-    visit 'localhost:4567'
+    visit '/'
     fill_in 'player1', with: 'Tom'
     fill_in 'player2', with: 'Brian'
     click_button 'Start Battle'
