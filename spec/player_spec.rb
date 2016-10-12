c8422b2432af6448061a10cc581003a0e1ea747d
Tomy8s <tomy8s@hotmail.com>
2016-10-12 14:44:23 +0100

    added tests for #name in new Player class

---
+++ spec/player_spec.rb
@@ -0,0 +1,10 @@
+require 'player'
+
+describe Player do
+  subject = Player.new('Brian')
+  describe '#name' do
+    it 'returns the player\'s name' do
+      expect(subject.name).to eq 'Brian'
+    end
+  end
+end
