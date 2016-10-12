f397f2dc737fc636e5117465c5918a531605d54d
amidabrian51 <briankaty1@me.com>
2016-10-12 10:34:16 +0100

    Added test for names

---
+++ spec/features/name_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+feature 'names' do
+  scenario 'Players can input names' do
+    visit 'localhost:4567'
+    fill_in 'player1', with: 'Tom'
+    fill_in 'player2', with: 'Brian'
+    click_button 'Start Battle'
+    expect(page).to have_content 'Tom vs Brian'
+  end
+end
