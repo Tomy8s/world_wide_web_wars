20f4048ab17f49888a48192f39cef1e6ce5cb4e4
amidabrian51 <briankaty1@me.com>
2016-10-12 16:24:39 +0100

    Added test for reduced score

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -7,5 +7,9 @@ feature 'attack' do
     click_button('Attack')
     expect(page).to have_content('You have hit Brian')
   end
-
+  scenario 'I want to reduce my opponents score' do
+    sign_in_and_play
+    click_button('Attack')
+    expect(page). to have_content 'Brian has 90HP'
+  end
 end
