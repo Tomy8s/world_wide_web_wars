f557b5f551ddeea857b9fb464c06be51146679fd
michkles <michkles@gmail.com>
2016-10-13 09:22:57 +0100

    fixes for game_spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-describe Game do
-
-	subject(:game) { described_class.new }
-	let(:player_1) { double :player }
-	let(:player_2) { double :player }
-
-	describe "#attack" do
-			it "damages the player" do
-				expect(player_2).to receive(:receive_damage)
-				game.attack(player)
-			end
-	end
-end
-=======
 require 'game'
 
 describe Game do
@@ -22,4 +7,3 @@ describe Game do
     end
   end
 end
->>>>>>> 0c48e6fd19f7ab404f8f37caac46917effa27891
