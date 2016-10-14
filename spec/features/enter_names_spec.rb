11b3301e80c22483701b7a91eacfba9cef98dd1a
hanrattyjen <hanratty.jen@gmail.com>
2016-10-14 10:53:32 +0100

    Added feature spec to add a computer player, should player_2 field be empty.

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -6,4 +6,11 @@ feature 'Enter names' do
     sign_in_and_play
     expect(page).to have_content ("Bob")
   end
+
+  scenario 'if player 2 field is empty, player 2 is the computer' do
+    visit('/')
+    fill_in :player_1_name, with: "Bob"
+    click_button 'Submit'
+    expect(page).to have_content ("Bob vs. Computer")
+  end
 end
